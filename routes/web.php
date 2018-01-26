5d70df6d0badcbe9955c965a65f42c5aac506f41
Adam Huang <adamhut@gmail.com>
2018-01-25 17:01:15 -0800

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Events\OrderStatusUpdated;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+class Order{
+    public $id;
+    public function __construct($id)
+    {
+        $this->id = $id;
+    }
+}
+Route::get('/', function () {
+    //event(new OrderStatusUpdated::class);
+
+    OrderStatusUpdated::dispatch(new Order(1));
+    return view('welcome');
+});
